8acc6b25477a443af664e18fd44fb0295c4587a1
Shobhit Dhamania (RIT Student) <sd5936@rit.edu>
2021-09-13 14:20:48 -0400

    half done

--- src/server.js
+++ src/server.js
@@ -1,2 +1,23 @@
 const http = require('http');
+const url = require('url');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
 
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  htmlHandler.getIndex(request, response);
+};
+
+const urltruct = {
+  'GET': {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    
+  },
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
